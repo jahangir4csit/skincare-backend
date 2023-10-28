c8f9d0682f281ef4ad9ad9d243df5a68cb2daf93
jahangir4csit <jahangir4csit@gmail.com>
2023-10-28 07:12:48 +0600

    api done

---
+++ seeder/users.js
@@ -0,0 +1,23 @@
+const bcrypt = require("bcryptjs")
+// const ObjectId = require("mongodb").ObjectId;
+const {ObjectId} = require('mongodb');
+const objectId = new ObjectId('625add3d78fb449f9d9fe2ee');
+
+const users = [
+      {
+    name: 'admin',
+    lastName: 'admin',
+    email: 'admin@admin.com',
+    password: bcrypt.hashSync('admin@admin.com', 10),
+    isAdmin: true,
+  },
+  {
+      _id: objectId,
+    name: 'John',
+    lastName: 'Doe',
+    email: 'john@doe.com',
+    password: bcrypt.hashSync('john@doe.com', 10),
+  },
+]
+
+module.exports = users
